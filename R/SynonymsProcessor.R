3bf3f70c003e0aa0e5f16aeccd3060b0bd6bfe33
Frank Wang <frankwang@Michals-iMac.uhn.ca>
2019-08-21 16:58:08 -0400

    Removed all chemical formulas from synonyms

--- R/SynonymsProcessor.R
+++ R/SynonymsProcessor.R
@@ -2,6 +2,9 @@ getUnique <- function(x){
   aliases <- as.character(drugs_with_ids[x, ])
   aliases <- aliases[!is.na(aliases)]
   aliases <- aliases[!grepl('/', aliases)]
+  aliases <- aliases[!grepl('{', aliases, fixed=TRUE)]
+  aliases <- aliases[!grepl('[', aliases, fixed=TRUE)]
+  aliases <- aliases[!grepl(',', aliases, fixed=TRUE)]
   aliases <- toupper(aliases)
   return(unique(aliases))
 }
